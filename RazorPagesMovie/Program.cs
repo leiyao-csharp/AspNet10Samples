e9111cff60cc91f57097048ea9a7104cd0b426e0
Lei Yao <26743502+leiyao-csharp@users.noreply.github.com>
2025-09-13 12:21:08 +0800

    Add database seeding functionality

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +16,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
 }
 
 app.UseHttpsRedirection();
