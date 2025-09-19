4b36127ecdf4d63affcf27bb6205448345be37f0
Lei Yao <26743502+leiyao-csharp@users.noreply.github.com>
2025-09-19 10:47:54 +0800

    Add a controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return $"Hello {name}, NumTimes is: {numTimes}";
+    }
+}
